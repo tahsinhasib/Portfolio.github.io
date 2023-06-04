a884f0b7d1741aac403cbe381dd10699199643f6
Tahsin Hasib <tahsinhasib@gmail.com>
2023-06-04 22:40:13 +0600

    last commit

--- js/script.js
+++ js/script.js
@@ -57,3 +57,26 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+
+//Dark mode
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+  darkModeIcon.classList.toggle('bx-sun');
+  document.body.classList.toggle('dark-mode');
+};
+
+
+//Scroll reveal
+ScrollReveal ({
+  reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.services-container, .projects-box, .certificates-wrapper, .contact form, .about-content', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h3, .about-img img', { origin: 'left' });
